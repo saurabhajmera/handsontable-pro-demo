12e97579888dab0480b2161fade0111ca0327c99
Saurabh Ajmera <sajmera@netflix.com>
2018-01-31 15:47:23 -0800

    test1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 
+// import {Handsontable} from 'handsontable-pro';
+
+declare var Handsontable: any;
+
+// const container = document.getElementById('example');
+// const hot = new Handsontable(container, {
+//   data: data,
+//   rowHeaders: true,
+//   colHeaders: true,
+//   filters: true,
+//   dropdownMenu: true
+// });
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'app';
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    const container = document.getElementById('example');
+    console.log(container);
+
+    const data = [
+      ['', 'Tesla', 'Volvo', 'Toyota', 'Honda'],
+      ['2017', 10, 11, 12, 13],
+      ['2018', 20, 11, 14, 13],
+      ['2019', 30, 15, 12, 13]
+    ];
+
+    const hot = new Handsontable(container, {
+      data: data,
+      rowHeaders: true,
+      colHeaders: true,
+      filters: true,
+      dropdownMenu: true
+    });
+
+
+  }
+
 }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,4 +1,5 @@
 /* SystemJS module definition */
+// declare module 'handsontable';
 declare var module: NodeModule;
 interface NodeModule {
   id: string;
