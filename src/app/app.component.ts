ef91d8114f5643ebb9cd155904182aaa270429ff
Saurabh Ajmera <sajmera@netflix.com>
2018-02-01 13:12:32 -0800

    nested headers is working.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,23 +9,28 @@ import {AfterViewInit, Component, OnInit} from '@angular/core';
 export class AppComponent implements OnInit, AfterViewInit {
   public data: any[];
   public options: any;
+  public headers: any[];
 
   constructor() {
     this.data = [
-      ['', 'Tesla', 'Volvo', 'Toyota', 'Honda'],
-      ['2017', 10, 11, 12, 13],
-      ['2018', 20, 11, 14, 13],
-      ['2019', 30, 15, 12, 13]
+      ['', 'X','S','3', 'Volvo', 'Toyota', 'Honda'],
+      ['2017',10, 11, 12, 10, 11, 12, 13],
+      ['2018',10, 11, 12, 20, 11, 14, 13],
+      ['2019',10, 11, 12, 30, 15, 12, 13]
     ];
 
+    this.headers = ['', 'Tesla', 'Volvo', 'Toyota', 'Honda'];
+
     this.options = {
-      colHeaders: true,
       rowHeaders: true,
       stretchH: 'all',
       columnSorting: true,
       contextMenu: true,
       filters: true,
-      dropdownMenu: true
+      dropdownMenu: true,
+      nestedHeaders: [
+        ['', {label: 'Tesla', colspan: 3}, 'Volvo', 'Toyota', 'Honda']
+      ]
     };
   }
 
